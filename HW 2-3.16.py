3c13a3337067217cea497a1f19e66abc3f4c3c87
Jensen Vieczorek <jrv220@lehigh.edu>
2022-07-18 22:39:01 -0400

    finished 3.16

--- HW 2-3.16.py
+++ HW 2-3.16.py
@@ -5,3 +5,26 @@ Created on Mon Jul 18 20:06:36 2022
 @author: jense
 """
 
+#this was hard and i wouldnt know how to do this without solution notes
+#range 2-10 works but so does range 8 which doesnt make sense, should be 9
+
+largest = int(input('Enter number: '))
+number = int(input('Enter number: '))
+
+if number > largest:
+    next_largest = largest
+    largest = number
+else:
+    next_largest = number
+
+for counter in range(8):
+    number = int(input('Enter number: '))
+
+    if number > largest:
+        next_largest = largest
+        largest = number
+    elif number > next_largest:
+        next_largest = number
+
+print(f'The largest number is {largest}')
+print(f'The second largest number is {next_largest}')
