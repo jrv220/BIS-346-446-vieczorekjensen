4b151908cbe1f8d442ff6353f3cf6dfff302edc5
Jensen Vieczorek <jrv220@lehigh.edu>
2022-07-18 21:44:25 -0400

    finished 3.6

--- HW 2-3.6.py
+++ HW 2-3.6.py
@@ -5,3 +5,22 @@ Created on Mon Jul 18 19:31:17 2022
 @author: jense
 """
 
+# these are my notes
+
+response = 'yes', 'no'
+
+input('Have you had this problem before (yes or no)?')
+if response == 'yes':
+    print('Well, you have it again.')
+if response == 'no':
+    print('Well, you have it now.')
+    
+
+# this is my final answer
+
+response = input('Have you had this problem before (yes or no)?')
+
+if response == 'yes':
+    print('Well, you have it again.')
+if response == 'no':
+    print('Well, you have it now.')
