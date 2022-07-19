4b90bbf99ee5359cbc2c4885498a3d77d5c89ccb
Jensen Vieczorek <jrv220@lehigh.edu>
2022-07-18 20:41:26 -0400

    finished 2.8

--- HW 2-2.8.py
+++ HW 2-2.8.py
@@ -4,4 +4,33 @@ Created on Mon Jul 18 20:04:26 2022
 
 @author: jense
 """
+# these are my notes to figure things out 
 
+print ('number'\t, 'square'\t, 'cube'\t)
+
+print ('number\t', 'square\t', 'cube\t')
+print ('number\t', 'square\t', 'cube\t')
+print('0\t', '0 ** 2\t', '0 ** 3\t')
+
+print ('number\t', 'square\t', 'cube\t')
+print(0 '\t', 0 ** 2 '\t', 0 ** 3 '\t')
+
+print ('number\t', 'square\t', 'cube')
+print(0, '\t', '\t', 0 ** 2, '\t', '\t', 0 ** 3, '\t')
+print(1, '\t', '\t', 1 ** 2, '\t', '\t', 1 ** 3, '\t')
+print(2, '\t', '\t', 2 ** 2, '\t', '\t', 2**3, '\t')
+print(3, '\t', '\t', 3 ** 2, '\t', '\t', 3 ** 3, '\t')
+print(4, '\t', '\t', 4 ** 2, '\t', 4 ** 3, '\t')
+print(5, '\t', '\t', 5 ** 2, '\t', 5 ** 3, '\t')
+
+# this is my final answer
+# does the code need to be the same as yours even if it gets same result
+
+
+print ('number\t', 'square\t', 'cube')
+print(0, '\t', '\t', 0 ** 2, '\t', '\t', 0 ** 3, '\t')
+print(1, '\t', '\t', 1 ** 2, '\t', '\t', 1 ** 3, '\t')
+print(2, '\t', '\t', 2 ** 2, '\t', '\t', 2**3, '\t')
+print(3, '\t', '\t', 3 ** 2, '\t', '\t', 3 ** 3, '\t')
+print(4, '\t', '\t', 4 ** 2, '\t', 4 ** 3, '\t')
+print(5, '\t', '\t', 5 ** 2, '\t', 5 ** 3, '\t')
