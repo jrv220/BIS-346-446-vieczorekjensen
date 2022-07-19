8986e9c78bc44c7326da59d49167cb7e1bf2de98
Jensen Vieczorek <jrv220@lehigh.edu>
2022-07-18 20:56:43 -0400

    finished 2.12

--- HW 2-2.12.py
+++ HW 2-2.12.py
@@ -5,3 +5,25 @@ Created on Mon Jul 18 20:05:17 2022
 @author: jense
 """
 
+# these are my notes for this problem
+
+p = 1000
+r = 0.07
+n = 10, 20, 30
+
+
+p(1 + r) ** 10
+
+print('amount on deposit at the end of 10 years:', p(1 + r) ** 10)
+print('amount on deposit at the end of 20 years:', p(1 + r) ** 20)
+print('amount on deposit at the end of 30 years:', p(1 + r) ** 30)
+
+print('amount on deposit at the end of 10 years:', 1000(1 + 0.07) ** 10)
+print('amount on deposit at the end of 20 years:', 1000(1 + 0.07) ** 20)
+print('amount on deposit at the end of 30 years:', 1000(1 + 0.07) ** 30)
+
+# this is my final answer
+
+print('amount on deposit at the end of 10 years:', 1000 * (1 + 0.07) ** 10)
+print('amount on deposit at the end of 20 years:', 1000 * (1 + 0.07) ** 20)
+print('amount on deposit at the end of 30 years:', 1000 * (1 + 0.07) ** 30)
