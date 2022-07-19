b4ae5ab0c0d81ef8b59908009e1c2051a11ac755
Jensen Vieczorek <jrv220@lehigh.edu>
2022-07-18 23:21:38 -0400

    finished ish 4.9

--- HW 2-4.9.py
+++ HW 2-4.9.py
@@ -5,3 +5,31 @@ Created on Mon Jul 18 20:06:56 2022
 @author: jense
 """
 
+#notes for this problem
+
+F = 'Farenheit'
+C = 'Celsius'
+F = (9 / 5) * C + 32
+
+
+
+farenheit range(101)
+
+
+for temperatures in range(101):
+    print('Farenheit', '\t', 'Celsius', '\t')
+    print(range(101), '\t', '\t', '\t', (9 // 5) * (range(101) + 32)
+          
+          
+          
+          
+# cannot figure out what im doing wrong but here is my answer
+# had it working until i tried to add headers to the columns then it all screwed
+#help
+          
+def farenheit(celsius):
+    """Calculate temperature degrees in farenheit from celsius"""
+    return (9 // 5) * celsius + 32
+
+for temperatures in range(-1, 101):
+    print(f'{celsius:10}{fahrenheit(celsius):10.0f}')
